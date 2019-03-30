53594687def3a887fc486363dd7d75560556cc49
dzelga <dorota.zelga@gmail.com>
2019-03-30 22:25:42 +0100

    default list added to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
+interface State {
+  list: Array<number>;
+}
+
+interface Props {
+}
+
+class App extends Component<Props, State> {
+  public state: State;
+  // public props: Props;
+  constructor(props : Props)  {
+    super(props);
+    this.state = {
+      list : []
+    }
+  }
+
+  componentWillMount() {
+    this.getDefaultList();
+  }
+
+  getDefaultList() {
+    const defaultList : Array<number> = [6, 5, 3, 1, 7, 8, 2, 4];
+    this.setState({
+      list : defaultList
+    });
+  }
+
   render() {
     return (
       <div className="App">
        <h1>Sorting Algorithms App</h1>
+       <p>{this.state.list}</p>
       </div>
     );
   }
