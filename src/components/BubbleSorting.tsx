ffaa7a0cc3442e888a2477611e3c462837a2a3f1
dzelga <dorota.zelga@gmail.com>
2019-03-30 23:12:52 +0100

    list sorted with use of  bubble sorting

--- src/components/BubbleSorting.tsx
+++ src/components/BubbleSorting.tsx
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
 
+interface State {
+    result: Array<number>
+}
+
 interface Props {
     list: Array<number>
 }
 
-class BubbleSorting extends Component<Props> {
+class BubbleSorting extends Component<Props, State> {
+    public state : State;
     constructor(){
         super();
-        this.state = {}
+        this.state = {
+            result : []
+        }
     }
+
+    componentWillMount() {
+        this.useBubbleSorting()
+    }
+
+    useBubbleSorting(){
+        var array : Array<number> = this.props.list;
+        for(var j = 0; j < array.length; j++) {
+            for(var i = 0; i < array.length; i++) {
+            if(array[i] > array[i+1]){
+                [array[i], array[i+1]] = [array[i+1], array[i]];
+            }
+        }
+        this.setState({ result : array })
+    }
+}
 
     render() {
         return <div>
             <h2>Bubble Sorting</h2>
+            {/* <p>{this.state.result}</p> */}
         </div>
     }
 }
