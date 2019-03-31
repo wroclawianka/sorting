6fe4ce010472bdadfaa58f2bcbdb74346f24ca75
dzelga <dorota.zelga@gmail.com>
2019-03-31 14:00:27 +0200

    styling with bootrstrap

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import './App.css';
 import BubbleSorting from './components/BubbleSorting/BubbleSorting';
 import SelectionSorting from './components/SelectionSorting/SelectionSorting';
 import InsertionSorting from './components/InsertionSorting/InsertionSorting';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 interface State {
   list: Array<number>;
@@ -33,15 +36,29 @@ class App extends Component<Props, State> {
 
   render() {
     return (
-      <div className="App">
-        <h1>Sorting Algorithms App</h1>
-        <h2>List of the elements:</h2>
-        <p>{this.state.list}</p>
-        <div>
-          <BubbleSorting list={this.state.list}></BubbleSorting>
-          <SelectionSorting list={this.state.list}></SelectionSorting>
-          <InsertionSorting list={this.state.list}></InsertionSorting>
-        </div>
+      <div>
+          <Container className="text-center" id="page-content">
+            <Row className="justify-content-center">
+              <Col className="col-md-7">
+                <h1 className="font-weight-light mt-4">Sorting Algorithms</h1>
+                <p className="lead">List of the elements</p>
+                <p className="lead">
+                  {this.state.list.map(function (element) {
+                    return <span className="list-element">{element}</span>
+                  })}
+                </p>
+              </Col>
+              </Row>
+              <Row className="justify-content-center">
+                <BubbleSorting list={this.state.list}></BubbleSorting>
+              </Row>
+              <Row className="justify-content-center">
+                <SelectionSorting list={this.state.list}></SelectionSorting>
+              </Row>
+              <Row className="justify-content-center">
+                <InsertionSorting list={this.state.list}></InsertionSorting>
+              </Row>
+          </Container>
       </div>
     );
   }
