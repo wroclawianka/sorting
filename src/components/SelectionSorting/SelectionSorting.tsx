f7c10abaaa0aa291b6c8a2444cf1b318edc9b330
dzelga <dorota.zelga@gmail.com>
2019-03-31 18:43:58 +0200

    visualization of steps - selection sorting

--- src/components/SelectionSorting/SelectionSorting.tsx
+++ src/components/SelectionSorting/SelectionSorting.tsx
@@ -1,21 +1,31 @@
 import React, { Component } from 'react';
+import './SelectionSorting.css';
+import posed, { PoseGroup } from 'react-pose';
+const Item = posed.li({});
 
 interface State {
     result: Array<number>,
-    steps: Array<any>
+    steps: Array<Step>,
+    items: Array<any>
 }
 
 interface Props {
     list: Array<number>
 }
 
+interface Step {
+    label: string,
+    array: Array<number>,
+    accent: Array<number>
+}
 class SelectionSorting extends Component<Props, State> {
     public state: State;
     constructor() {
         super();
         this.state = {
             result: [],
-            steps: []
+            steps: [],
+            items: []
         }
     }
 
@@ -23,28 +33,55 @@ class SelectionSorting extends Component<Props, State> {
         this.useSelectionSorting()
     }
 
+    showSteps(i) {
+        setTimeout(() => {
+            this.setState({
+                items: this.state.steps[i].array
+            })
+            i++;
+            if (i < this.state.steps.length) {
+                this.showSteps(i);
+            }
+        }, 1000);
+    }
+
     useSelectionSorting() {
-        var array: Array<number> = [...this.props.list];
-        var steps = [[...array]];
+        let array: Array<number> = [...this.props.list];
+        var startStep: Step = {
+            label: "Start",
+            array: [...array],
+            accent: []
+        };
+        var steps: Array<Step> = [
+            startStep
+        ];
         for (var i = 0; i < array.length; i++) {
             let min = Math.min(...array.slice(i, array.length));
             let minIndex = array.indexOf(min);
             [array[i], array[minIndex]] = [array[minIndex], array[i]];
-            steps.push([...array])
+            steps.push({
+                label: `Step ${i + 1}`,
+                array: [...array],
+                accent: [i, minIndex]
+            });
         }
         this.setState({
             result: array,
             steps: steps
-        })
+        }, () => {this.showSteps(0)})
     }
 
     render() {
         return <div>
             <h2>Selection Sorting</h2>
             <ul>
-                {this.state.steps.map(function (step: []) {
-                    return <li>{step}</li>;
-                })}
+                <PoseGroup>
+                    {this.state.items.map(item =>
+                        <Item key={item} className="item">
+                            <span>{item}</span>
+                        </Item>
+                    )}
+                </PoseGroup>
             </ul>
         </div>
     }
