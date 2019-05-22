bb63a658193dbb8532bdb7ae72d41afb3eb8bbfa
dzelga <dorota.zelga@gmail.com>
2019-05-22 21:37:06 +0200

    Bubble > algorithm improvement

--- src/components/BubbleSorting/BubbleSorting.tsx
+++ src/components/BubbleSorting/BubbleSorting.tsx
@@ -49,7 +49,7 @@ class BubbleSorting extends Component<Props, State> {
         let steps = [[...array]];
         let sortedArray = [...array].sort();
         for (let j = 0; j < array.length; j++) {
-            for (let i = 0; i < array.length; i++) {
+            for (let i = 0; i < array.length - 1; i++) {
                 if (JSON.stringify(array) !== JSON.stringify(sortedArray)) {
                     if (array[i] > array[i + 1]) {
                         [array[i], array[i + 1]] = [array[i + 1], array[i]];
