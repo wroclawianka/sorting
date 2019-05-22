ebb7ffd1f1cbd5b95f6de11ba5d7827aea54b4a1
dzelga <dorota.zelga@gmail.com>
2019-05-22 20:47:33 +0200

    Bubble > stop sorting if already sorted

--- src/components/BubbleSorting/BubbleSorting.tsx
+++ src/components/BubbleSorting/BubbleSorting.tsx
@@ -45,12 +45,15 @@ class BubbleSorting extends Component<Props, State> {
     useBubbleSorting() {
         var array: Array<number> = [...this.props.list];
         var steps = [[...array]];
+        var sortedArray = [...array].sort();
         for (var j = 0; j < array.length; j++) {
             for (var i = 0; i < array.length; i++) {
-                if (array[i] > array[i + 1]) {
-                    [array[i], array[i + 1]] = [array[i + 1], array[i]];
-                }
-                steps.push([...array])
+                if(JSON.stringify(array) !== JSON.stringify(sortedArray)){
+                    if (array[i] > array[i + 1]) {
+                        [array[i], array[i + 1]] = [array[i + 1], array[i]];
+                    }
+                    steps.push([...array]);
+               }
             }
             this.setState({
                 result: array,
@@ -61,7 +64,7 @@ class BubbleSorting extends Component<Props, State> {
 
     render() {
         return <div>
-            <h2 className="font-weight-light mt-4">Bubble Sorting</h2>
+            <h2 className="font-weight-light mt-4">Bubble Sorting {this.state.stepsCounter}</h2>
             <ul>
                 <PoseGroup>
                     {this.state.items.map(item =>
