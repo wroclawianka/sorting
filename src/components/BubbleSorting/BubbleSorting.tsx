875b72626e1cf41f5aa9d57551b0887b90ef082a
dzelga <dorota.zelga@gmail.com>
2019-05-23 12:20:25 +0200

    Bubble > accent > compared elements accented

--- src/components/BubbleSorting/BubbleSorting.tsx
+++ src/components/BubbleSorting/BubbleSorting.tsx
@@ -7,7 +7,9 @@ interface State {
     result: Array<number>,
     steps: Array<any>,
     items: Array<number>,
-    stepsCounter: number
+    stepsCounter: number,
+    comparedElements : Array<Array<number>>,
+    indexes : Array<number>
 }
 
 interface Props {
@@ -23,15 +25,22 @@ class BubbleSorting extends Component<Props, State> {
             result: [],
             steps: [],
             items: [],
-            stepsCounter: 0
+            stepsCounter: 0,
+            comparedElements : [],
+            indexes : [0, 1]
         }
     }
 
     showSteps(i) {
         setTimeout(() => {
+            let indexes : Array<number> = [];
+            this.state.comparedElements[i].forEach(el => {
+                indexes.push(this.state.steps[i].indexOf(el));
+            });
             this.setState({
                 items: this.state.steps[i],
-                stepsCounter: i
+                stepsCounter: i,
+                indexes: indexes
             });
             i++;
             if (i < this.state.steps.length) {
@@ -48,9 +57,11 @@ class BubbleSorting extends Component<Props, State> {
         const array: Array<number> = [...this.props.list];
         let steps = [[...array]];
         let sortedArray = [...array].sort();
+        let comparedElements : Array<Array<number>> = [[0,0]];
         for (let j = 0; j < array.length; j++) {
             for (let i = 0; i < array.length - 1; i++) {
                 if (JSON.stringify(array) !== JSON.stringify(sortedArray)) {
+                    comparedElements.push([array[i], array[i + 1]]);
                     if (array[i] > array[i + 1]) {
                         [array[i], array[i + 1]] = [array[i + 1], array[i]];
                     }
@@ -59,11 +70,24 @@ class BubbleSorting extends Component<Props, State> {
             }
             this.setState({
                 result: array,
-                steps: steps
+                steps: steps,
+                comparedElements: comparedElements
             }, () => {
                 this.showSteps(0)
             })
         }
+    }
+
+    setAccent(item) {
+        let i = this.state.stepsCounter;
+        let index = this.state.steps[i].indexOf(item);
+        let className = "item";
+        this.state.indexes.forEach(el => {
+            if(el === index) {
+                className = className + " accented"
+            }
+        });
+        return className
     }
 
     render() {
@@ -73,7 +97,7 @@ class BubbleSorting extends Component<Props, State> {
             <ul>
                 <PoseGroup>
                     {this.state.items.map(item =>
-                        <Item key={item} className="item">
+                        <Item key={item} className={this.setAccent(item)}>
                             <span>{item}</span>
                         </Item>
                     )}
