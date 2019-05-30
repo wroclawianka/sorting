9dd127d67f156e62c2acf4aa82f81d841962b587
dzelga <dorota.zelga@gmail.com>
2019-05-30 20:07:51 +0200

    Sorting algorithm presentation moved to SortingDisplay component

--- src/components/BubbleSorting/BubbleSorting.tsx
+++ src/components/BubbleSorting/BubbleSorting.tsx
@@ -1,15 +1,11 @@
 import React, {Component} from 'react';
-import posed, {PoseGroup} from 'react-pose';
-
-const Item = posed.li({});
+import SortingDisplay from '../SortingDisplay/SortingDisplay';
 
 interface State {
     result: Array<number>,
     steps: Array<any>,
     items: Array<number>,
-    stepsCounter: number,
-    comparedElements : Array<Array<number>>,
-    indexes : Array<number>
+    comparedElements: Array<Array<number>>,
 }
 
 interface Props {
@@ -25,28 +21,8 @@ class BubbleSorting extends Component<Props, State> {
             result: [],
             steps: [],
             items: [],
-            stepsCounter: 0,
-            comparedElements : [],
-            indexes : [0, 1]
+            comparedElements: []
         }
-    }
-
-    showSteps(i) {
-        setTimeout(() => {
-            let indexes : Array<number> = [];
-            this.state.comparedElements[i].forEach(el => {
-                indexes.push(this.state.steps[i].indexOf(el));
-            });
-            this.setState({
-                items: this.state.steps[i],
-                stepsCounter: i,
-                indexes: indexes
-            });
-            i++;
-            if (i < this.state.steps.length) {
-                this.showSteps(i);
-            }
-        }, 1000);
     }
 
     componentWillMount() {
@@ -57,7 +33,7 @@ class BubbleSorting extends Component<Props, State> {
         const array: Array<number> = [...this.props.list];
         let steps = [[...array]];
         let sortedArray = [...array].sort();
-        let comparedElements : Array<Array<number>> = [[0,0]];
+        let comparedElements: Array<Array<number>> = [[0, 0]];
         for (let j = 0; j < array.length; j++) {
             for (let i = 0; i < array.length - 1; i++) {
                 if (JSON.stringify(array) !== JSON.stringify(sortedArray)) {
@@ -74,38 +50,18 @@ class BubbleSorting extends Component<Props, State> {
                 result: array,
                 steps: steps,
                 comparedElements: comparedElements
-            }, () => {
-                this.showSteps(0)
             })
         }
     }
 
-    setAccent(item) {
-        let i = this.state.stepsCounter;
-        let index = this.state.steps[i].indexOf(item);
-        let className = "item";
-        this.state.indexes.forEach(el => {
-            if(el === index) {
-                className = className + " accented"
-            }
-        });
-        return className
-    }
-
     render() {
-        return <div>
-            <h2 className="font-weight-light mt-4">Bubble Sorting</h2>
-            <span className="counter">{this.state.stepsCounter}</span>
-            <ul>
-                <PoseGroup>
-                    {this.state.items.map(item =>
-                        <Item key={item} className={this.setAccent(item)}>
-                            <span>{item}</span>
-                        </Item>
-                    )}
-                </PoseGroup>
-            </ul>
-        </div>
+        return <SortingDisplay
+            name={"Bubble Sorting"}
+            result={this.state.result}
+            steps={this.state.steps}
+            items={this.state.items}
+            comparedElements={this.state.comparedElements}
+        />
     }
 }
 
