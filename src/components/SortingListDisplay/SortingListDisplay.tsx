358c8eb2f8e27c41944ed4868902aa73afa0aeea
dzelga <dorota.zelga@gmail.com>
2019-06-15 18:20:55 +0200

    SortingListDisplay > component created

---
+++ src/components/SortingListDisplay/SortingListDisplay.tsx
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+interface State {
+}
+
+interface Props {
+}
+
+class SortingListDisplay extends Component<Props, State> {
+    state: State;
+
+    constructor() {
+        super();
+        this.state = {}
+    }
+
+    render() {
+        return <div>
+        </div>
+    }
+}
+
+export default SortingListDisplay;
